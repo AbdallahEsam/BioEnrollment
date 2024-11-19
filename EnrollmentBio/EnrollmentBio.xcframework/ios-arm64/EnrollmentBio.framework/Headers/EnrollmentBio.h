3536658356b1e93ccce169bcde55a2c61bb3fb4f
Abdallah Essam Shaban <ashaban@saudivts.com>
2024-11-19 11:28:05 +0300

    first commit

---
+++ EnrollmentBio/EnrollmentBio.xcframework/ios-arm64/EnrollmentBio.framework/Headers/EnrollmentBio.h
@@ -0,0 +1,18 @@
+//
+//  EnrollmentBio.h
+//  EnrollmentBio
+//
+//  Created by Abdallah Essam Shaban on 19/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EnrollmentBio.
+FOUNDATION_EXPORT double EnrollmentBioVersionNumber;
+
+//! Project version string for EnrollmentBio.
+FOUNDATION_EXPORT const unsigned char EnrollmentBioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnrollmentBio/PublicHeader.h>
+
+
